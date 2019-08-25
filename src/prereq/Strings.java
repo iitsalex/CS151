61046f5d9714d3f9eca16882b265564a56699732
Alexander Khalilieh <iitsalex@hotmail.com>
2019-08-24 19:17:45 -0700

    Initial Commit + Strings

---
+++ src/prereq/Strings.java
@@ -0,0 +1,18 @@
+package prereq;
+
+public class Strings
+{
+   public static String uniqueLetters(String str)
+   {
+	   String endResult = str;
+	   char a = 0;
+	   for (int i = 0; i<str.length();i++) 
+	   {
+		   for (int l = 0; l<str.length();l++) {
+			   if (str.charAt(l) == str.charAt(i) && l != i)
+				   endResult = endResult.replace(str.charAt(l),a);
+		   }
+	   }
+	   return endResult.replace("\0", "");
+   }
+}
